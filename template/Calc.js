b51418726cbf763200cde1f0851fd1615df93314
redFeather8888 <kuro.kumo2005@gmail.com>
2025-12-11 21:12:48 +0900

    Add files via upload

--- template/Calc.js
+++ template/Calc.js
@@ -1,72 +1,65 @@
+document.getElementById("button").addEventListener("click", () => {
+    OnButtonClick();
+});
 function OnButtonClick() {
-    //target = document.getElementById("budget");
-    //target.value = "2025";
     main()
 }
-
 function main(){
     //ListItemPriceとListItemIDはセットで扱う
-    ListItemPrice=[];//単品価格
-    ListItemID=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
-    ListPurchased=[];//購入が確定している商品の価格
+    let ListItemPrice=[];//単品価格
+    const ListItemID=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    let ListPurchased=[];//購入が確定している商品の価格
     //ListAllPatternとListAllPriceはセットで扱う
-    ListAllPattern=[];//全ての組み合わせ
-    ListAllPrice=[];//全ての組み合わせの金額
-    ListResult1st=[];//ピッタリな組み合わせ
-    ListResult2nd=[];//次にピッタリ
-    ListResult3rd=[];//次の次にピッタリ
+    let ListAllPattern=[];//全ての組み合わせ
+    let ListAllPrice=[];//全ての組み合わせの金額
+    let ListResult1st=[];//ピッタリな組み合わせ
+    let ListResult2nd=[];//次にピッタリ
+    let ListResult3rd=[];//次の次にピッタリ
 
-    budget=0;//予算
-    sumPurchased=0;//購入が確定している商品の合計金額
-    maxSum=0;//上限金額
+    let budget=0;//予算
+    let sumPurchased=0;//購入が確定している商品の合計金額
+    let maxSum=0;//上限金額
 
-    if(getInputData()==true){//入力内容が正しい
-        for(var i=0; i<ListPurchased.length; i++){//購入が確定している商品の合計金額
+    if(getInputData(ListItemPrice, ListPurchased)==true){//入力内容が正しい
+        for(let i=0; i<ListPurchased.length; i++){//購入が確定している商品の合計金額
             sumPurchased=sumPurchased+ListPurchased[i];
         }
         budget=document.getElementById("budget").value;//予算
         maxSum=budget-sumPurchased;//上限金額を計算する
-        calcAllPattern(maxSum);
-        calcResult(maxSum);
-        showResult();
-    }else{
-
+        calcAllPattern(maxSum, ListItemID, ListItemPrice, ListAllPattern, ListAllPrice);
+        calcResult(maxSum, ListAllPattern, ListAllPrice, ListResult1st, ListResult2nd, ListResult3rd);
+        showResult(ListItemID, ListItemPrice, ListResult1st, ListResult2nd, ListResult3rd);
     }
 }
-
-function getInputData(){//入力データを確認して配列に入れる
-    flag=true;
+function getInputData(ListItemPrice, ListPurchased){//入力データを確認して配列に入れる
+    let flag=true;
     const itemTable=document.getElementById("itemPrice");
     const purchasedTable=document.getElementById("purchasedPrice");
 
-    for (var i=1; i < itemTable.rows.length; i++) {//単品価格
-        itemPriceElm=itemTable.rows[i].cells[0].children[0];
-        itemPrice=Number(itemPriceElm.value);
+    for (let i=1; i < itemTable.rows.length; i++) {//単品価格
+        const itemPriceElm=itemTable.rows[i].cells[0].children[0];
+        const itemPrice=Number(itemPriceElm.value);
 
         if(Number.isInteger(itemPrice)==true){//単品価格が自然数
             if(itemPrice>0){
                 ListItemPrice.push(itemPrice);
             }
-
         }else{
             flag=false;
         }
     }
-
-    for (var i=1; i < purchasedTable.rows.length; i++) {//購入リスト
-        purchasedPriceElm=purchasedTable.rows[i].cells[0].children[0];
-        purchasedPrice=Number(purchasedPriceElm.value);
+    for (let i=1; i < purchasedTable.rows.length; i++) {//購入リスト
+        const purchasedPriceElm=purchasedTable.rows[i].cells[0].children[0];
+        const purchasedPrice=Number(purchasedPriceElm.value);
 
         if(Number.isInteger(purchasedPrice)==true){//購入商品価格が自然数
             if(purchasedPrice>0){
                 ListPurchased.push(purchasedPrice);
             }
-
         }else{
             flag=false;
         }
     }
-    
     if(ListItemPrice.length>0){
         AscendingOrder(ListItemPrice);
         DeleteDuplicate(ListItemPrice);
@@ -88,21 +81,19 @@ function AscendingOrder(array){//昇順に並び替え
     }
 }
 function DeleteDuplicate(array){//昇順・降順の配列から重複を消す
-    for(var i=array.length-1; i >0 ; i--){
+    for(let i=array.length-1; i >0 ; i--){
         if(array[i] == array[i-1]){
             array.splice(i,1);
         }
     }
 }
-
-function calcAllPattern(maxSum){//全パターンを計算する
+function calcAllPattern(maxSum, ListItemID, ListItemPrice, ListAllPattern, ListAllPrice){//全パターンを計算する
     Sum=0;
     pattern="X";
 
     if(maxSum<ListItemPrice[0]){//使える金額が単品の最小価格よりも小さい場合中断
         return false;
     }
-
     do{
         if(!(pattern.slice(-1)=="A")){//末端がAではないなら末尾に文字をつける
             if(pattern=="X"){
@@ -112,12 +103,10 @@ function calcAllPattern(maxSum){//全パターンを計算する
                 pattern=pattern+pattern.slice(-1);//末尾の文字
             }
         }
-
         Sum=0;
-        for(var i=0; i<pattern.length; i++){//合計金額を計算する
+        for(let i=0; i<pattern.length; i++){//合計金額を計算する
             Sum=Sum+ListItemPrice[ListItemID.indexOf(pattern.slice(i, i+1))];
-        }
-        
+        }  
         while(Sum>maxSum || ListAllPattern.indexOf(pattern)>-1){//合計金額が上限金額より高いorパターンリストに含まれている間ループ
             if(pattern.slice(-1)=="A"){
                 pattern=pattern.slice(0, pattern.length - 1);//末端を消す
@@ -125,7 +114,7 @@ function calcAllPattern(maxSum){//全パターンを計算する
             pattern=pattern.slice(0,pattern.length-1)+ListItemID[ListItemID.indexOf(pattern.slice(-1))-1];//末尾の文字を１つずらす
 
             Sum=0;
-            for(var i=0; i<pattern.length; i++){//合計金額を計算する
+            for(let i=0; i<pattern.length; i++){//合計金額を計算する
                 Sum=Sum+Number(ListItemPrice[ListItemID.indexOf(pattern.slice(i, i+1))]);
             }
         }
@@ -135,8 +124,7 @@ function calcAllPattern(maxSum){//全パターンを計算する
     
     return true;
 }
-
-function calcResult(maxSum){//条件に合うパターンを計算する
+function calcResult(maxSum, ListAllPattern, ListAllPrice, ListResult1st, ListResult2nd, ListResult3rd){//条件に合うパターンを計算する
     DeleteDuplicate(ListAllPrice);//取りうる金額のパターンを調べる
     arr=ListAllPrice.slice();
     AscendingOrder(arr);
@@ -152,43 +140,39 @@ function calcResult(maxSum){//条件に合うパターンを計算する
         sum2nd=arr[arr.length-1];
         sum3rd=arr[arr.length-2];
     }
-
-    for(var i=0; i<ListAllPrice.length; i++){
+    for(let i=0; i<ListAllPrice.length; i++){
         if(ListAllPrice[i]==sum1st){
             ListResult1st.push(ListAllPattern[i]);
         }
     }
-    for(var i=0; i<ListAllPrice.length; i++){
+    for(let i=0; i<ListAllPrice.length; i++){
         if(ListAllPrice[i]==sum2nd){
             ListResult2nd.push(ListAllPattern[i]);
         }
     }
-    for(var i=0; i<ListAllPrice.length; i++){
+    for(let i=0; i<ListAllPrice.length; i++){
         if(ListAllPrice[i]==sum3rd){
             ListResult3rd.push(ListAllPattern[i]);
         }
     }
 }
-
-function showResult(){//結果を表示する
-    resutlTable=document.getElementById("result");
-    num=0;
-    function addTableData(num, data, colorCode){
-        let tr = document.createElement("tr");
-        let td1 = document.createElement("td");
+function showResult(ListItemID, ListItemPrice, ListResult1st, ListResult2nd, ListResult3rd){//結果を表示する
+    function addTableData(num, data, colorCode, Fragment){
+        const td1 = document.createElement("td");
         td1.innerText = num;
         td1.style.textAlign="center"
-        let td2 = document.createElement("td");
+        const td2 = document.createElement("td");
         td2.innerText = data;
+        const tr = document.createElement("tr");
         tr.appendChild(td1);
         tr.appendChild(td2);
         tr.style.backgroundColor=colorCode
-        resutlTable.appendChild(tr);
+        Fragment.appendChild(tr);
     }
     function convID(ID){
-        var i=0;
-        var j=1
-        var str="";
+        let i=0;
+        let j=1
+        let str="";
         do{
             i=j;
             do{
@@ -203,21 +187,24 @@ function showResult(){//結果を表示する
 
         return str;
     }
-    var rowNum=resutlTable.rows.length;
-    for(var i=rowNum-1; i>0; i--){
+    const resutlTable=document.getElementById("result");
+    let num=0;
+    const rowNum=resutlTable.rows.length;
+    for(let i=rowNum-1; i>0; i--){
         resutlTable.deleteRow(i);
     }
-
-    for(var i=0; i<ListResult1st.length; i++){
+    const ElmFrag = document.createDocumentFragment();
+    for(let i=0; i<ListResult1st.length; i++){
         num++;
-        addTableData(num, convID(ListResult1st[i]),"#87cefa");
+        addTableData(num, convID(ListResult1st[i]),"#87cefa", ElmFrag);
     }
-    for(var i=0; i<ListResult2nd.length; i++){
+    for(let i=0; i<ListResult2nd.length; i++){
         num++;
-        addTableData(num, convID(ListResult2nd[i]),"#ffffe0");
+        addTableData(num, convID(ListResult2nd[i]),"#ffffe0", ElmFrag);
     }
-    for(var i=0; i<ListResult3rd.length; i++){
+    for(let i=0; i<ListResult3rd.length; i++){
         num++;
-        addTableData(num, convID(ListResult3rd[i]),"#f08080");
+        addTableData(num, convID(ListResult3rd[i]),"#f08080", ElmFrag);
     }
+    resutlTable.append(ElmFrag);
 }
